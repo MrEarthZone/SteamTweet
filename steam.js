7fbc265f6c5bc1fc282fe3be38d256a7a2895c58
Earth.Dev <sirom2010@gmail.com>
2018-03-19 16:01:35 +0700

    Update routing

--- steam.js
+++ steam.js
@@ -23,16 +23,16 @@ io.on('connection', function (socket) {
     console.log('a user connected');
 });
 
-app.use("/",express.static(__dirname));
+app.use("/",express.static("./"));
 
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/index2', function (req, res) {
     res.sendFile(__dirname + '/index2.html');
-});
+});*/
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
